841aaa81aeb9161b384e802c220ec60b044eb3e8
Tom White <tom@sixdozen.com>
2015-11-22 11:38:56 -0800

    release: 0.2.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='Kerosene',
-      version='0.1.1',
+      version='0.2.0',
       description='Simple wrapper for fuel datasets',
       author='Tom White',
       author_email='tom@sixdozen.com',
       url='https://github.com/dribnet/kerosene',
-      download_url='https://github.com/dribnet/kerosene/tarball/0.1.0',
+      download_url='https://github.com/dribnet/kerosene/tarball/0.2.0',
       license='MIT',
       install_requires=['fuel'],
       packages=find_packages())
